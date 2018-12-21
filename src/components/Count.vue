ecb619f8887063e3054e3f1248d6884439a4b39f
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2018-12-21 19:14:50 +0900

    [add] 標準入力を受け取るようにした

---
+++ src/components/Count.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <div>{{ count }}回クリック</div>
+    <button v-on:click="increment">カウントを増やす</button>
+    <div class='counter'>Count</div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Count",
+};
+</script>
+
+<style scoped>
+.counter {
+    font-size: 26px;
+    background-color: #dfd;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <input :value="message" placeholder="追加するタスクを入力">
+    <p>Message is: {{ message }}</p>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Count",
+};
+</script>
+
+<style scoped>
+.counter {
+    font-size: 26px;
+    background-color: #dfd;
+}
+</style>
